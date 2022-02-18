f90f93fc6b4ab962a1ce3087be7d44e160b636d8
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-02-18 20:26:40 +0800

    Flipping the cards

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import SingleCard from './components/SingleCard'
+import { useState, useEffect } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
-]
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
 
 function App() {
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards for new game
   const shuffleCards = () => {
     const cards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map(card => ({ ...card, id: Math.random() }))
-      
-    setCards(cards)
-    setTurns(0)
-  }
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(cards);
+    setTurns(0);
+  };
 
   // handle a choice
   const handleChoice = (card) => {
-    console.log(card)
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   // compare 2 selected cards
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-
       if (choiceOne.src === choiceTwo.src) {
-        console.log('those cards match')
-        resetTurn()
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
       } else {
-        console.log('those cards do not match')
-        resetTurn()
+        setTimeout(() => resetTurn(), 1000);
       }
-
     }
-  }, [choiceOne, choiceTwo])
-
+  }, [choiceOne, choiceTwo]);
+  console.log(cards);
   // reset choices & increase turn
   const resetTurn = () => {
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(prevTurns => prevTurns + 1)
-  }
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   return (
     <div className="App">
@@ -61,17 +66,17 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard 
+        {cards.map((card) => (
+          <SingleCard
             key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
           />
         ))}
       </div>
-
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,20 +1,23 @@
 import "./SingleCard.css";
 
-export default function SingleCard({ card, handleChoice }) {
-  
+export default function SingleCard({ card, handleChoice, flipped }) {
+  const isFlipped = flipped || card.matched;
+
   const handleClick = () => {
-    handleChoice(card)
-  }
-  
+    handleChoice(card);
+  };
+
   return (
-    <div>
-      <img className="front" src={card.src} alt="card front" />
-      <img
-        className="back"
-        src="/img/cover.png"
-        onClick={handleClick}
-        alt="card back"
-    />
+    <div className="card">
+      <div className={isFlipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="cover"
+        />
+      </div>
     </div>
   );
 }
